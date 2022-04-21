13297a275f6bf57d5fbb32ec1179c1a53e3b51c4
diego vallejo <vallejodiego@live.com>
2022-04-20 23:42:05 -0300

    Estructura del web, diego

---
+++ app.js
@@ -0,0 +1,14 @@
+//require
+
+const express = require("express");
+
+//variables 
+
+const app = express();
+
+//elServidor
+
+app.listen(3030, () => {console.log('Servidor corriendo')});
+
+// los Get:
+
