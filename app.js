ddeb1e35c9ef0e9b415396b497934cd05768cadc
diego vallejo <vallejodiego@live.com>
2022-04-25 14:23:39 -0300

    todo lo nuevo

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 //require
-
 const express = require("express");
-
+const path = require("path");
 //variables 
-
 const app = express();
+//Paths
+const publicPath = path.resolve(__dirname, './public') ;
+
+//seteo de archivos estaticos
+
+app.use( express.static(publicPath) );
 
 //elServidor
-
 app.listen(3030, () => {console.log('Servidor corriendo')});
 
 // los Get:
 
+app.get('/', (req, res)=>{
+    res.sendFile('./views/home.html')
+})
