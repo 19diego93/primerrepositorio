81e64e3c8196ba39d1b631c24ec9e7b59b14ae08
diego vallejo <vallejodiego@live.com>
2022-04-25 19:05:14 -0300

    enlaces , imagenes y heroes

--- app.js
+++ app.js
@@ -12,9 +12,10 @@ const elIndex = path.resolve(__dirname,'./DH-Heroes/views/index.html');
 const elBabbage = path.resolve(__dirname, './DH-Heroes/views/babbage.html');
 const elBernersLee = path.resolve(__dirname, './DH-Heroes/views/berners-lee.html');
 const elClarke = path.resolve(__dirname, './DH-Heroes/views/clarke.html');
-const elHamilton = path.resolve(__dirname, './DH-Heroes/views/hamilton.html');
-const elHopper = path.resolve(__dirname, './DH-Heroes/views/hamilton.html');
-const elLovelace = path.resolve(__dirname, './DH-Heroes/views/hamilton.html');
+const laHamilton = path.resolve(__dirname, './DH-Heroes/views/hamilton.html');
+const laHopper = path.resolve(__dirname, './DH-Heroes/views/hamilton.html');
+const laLovelace = path.resolve(__dirname, './DH-Heroes/views/lovelace.html');
+const elTuring = path.resolve(__dirname, './DH-Heroes/views/turing.html');
 
 //seteo de archivos estaticos
 app.use( express.static(publicPath) );
@@ -24,11 +25,11 @@ app.listen(3030, () => {console.log('Servidor corriendo')});
 
 // los Get:
 
-app.get('/home',(req, res) => {
-    res.sendFile(path.resolve(__dirname, './DH-Heroes/views/home.html'));
+app.get('/juan',(req, res) => {
+    res.sendFile(path.resolve(__dirname, './DH-Heroes/views/juan.html'));
 });
 
-app.get('/', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile(elIndex);
 });
 app.get('/babbage',(req, res) => {
@@ -41,14 +42,14 @@ app.get('/clarke',(req, res) => {
     res.sendFile(elClarke)
 });
 app.get('/hamilton',(req, res) => {
-    res.sendFile(elHamilton)
+    res.sendFile(laHamilton)
 });
 app.get('/hopper',(req, res) => {
-    res.sendFile(elHopper)
+    res.sendFile(laHopper)
 });
 app.get('/lovelace',(req, res) => {
-    res.sendFile(elLovelace)
+    res.sendFile(laLovelace)
 });
 app.get('/turing',(req, res) => {
-    res.sendFile(path.resolve(__dirname, './DH-Heroes/views/turing.html'))
+    res.sendFile(elTuring)
 });
